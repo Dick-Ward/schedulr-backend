06518d54c281ffb6f3a338fe71ee7c15a9edd660
Gonzodamus <Dick@TheDickWard.com>
2017-12-20 17:26:11 -0500

    API routes complete

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,38 @@
 class AppointmentsController < ApplicationController
+
+  def index
+    appointments = Appointment.all
+    render json: appointments
+  end
+
+  def show
+    appointment = Appointment.find(params[:id])
+    render json: appointment
+  end
+
+  def create
+    appointment = Appointment.new(appointment_params)
+    render json: appointment
+  end
+
+  def update
+    appointment = Appointment.find(params[:id])
+    appointment.update(appoinment_params)
+    render json: appointment
+  end
+
+  def destroy
+    appointment = Appointment.find(params[:id])
+    appointment.delete
+    render json: appointments
+  end
+
+
+
+private
+
+def appointment_params
+  params.require(:appointment).permit(:user_id, :street_address, :city, :date_time, :title, :duration, :description)
+end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  def create
+    user = User.new(user_params)
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(appoinment_params)
+    render json: user
+  end
+
+  def destroy
+    users = User.all
+    user = User.find(params[:id])
+    user.delete
+    render json: users
+  end
+
 end
