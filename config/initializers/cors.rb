9b38bfbbfae5933e03dc0aa61904802c639869cd
Gonzodamus <Dick@TheDickWard.com>
2017-12-31 11:57:51 -0500

    changing cors to run with localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://fathomless-forest-91982.herokuapp.com'
+    origins '*'
 
     resource '*',
       headers: :any,
